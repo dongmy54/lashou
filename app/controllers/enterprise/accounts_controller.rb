a6235381d1016f04c1b70bada236b46ec430499d
dongmy54 <615760433@qq.com>
2018-09-16 18:07:51 +0800

    add require_login to enterprise::accounts

--- app/controllers/enterprise/accounts_controller.rb
+++ app/controllers/enterprise/accounts_controller.rb
@@ -1,8 +1,15 @@
 class Enterprise::AccountsController < EnterprisesController
+  before_action :require_login
 
   # 后台概览
   def overview
   end
+
+  private
+
+    def require_login
+      redirect_to enterprise_account_login_path unless current_account
+    end
 
 
 end
