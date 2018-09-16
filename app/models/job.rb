c4976b91fd0eb9329d20756f0533b1756fde8e89
dongmy54 <615760433@qq.com>
2018-09-16 20:53:06 +0800

    job model add valid

--- app/models/job.rb
+++ app/models/job.rb
@@ -21,5 +21,23 @@
 # 职位
 class Job < ApplicationRecord
   belongs_to :company
-  
+
+  # 类型
+  Type             = %w(全职 兼职).freeze
+  # 学历
+  Education        = %w(小学 初中 高中 专科 本科 硕士以上).freeze
+  # 经验
+  WorkerExperience = %w(无要求 1-3年 3-5年 5年以上).freeze
+
+  validates_presence_of :name, :education, :type, :education, :worker_experience,
+                        :start_salary, :end_salary, :job_desc
+
+  validates_length_of :name,     in: 1..30
+  validates_length_of :job_desc, in: 1..500
+
+  validates :start_salary, :end_salary, numericality: {greater_than: 0, less_than: 9999}
+
+  validates_inclusion_of :type,              in: Type
+  validates_inclusion_of :education,         in: Education
+  validates_inclusion_of :worker_experience, in: WorkerExperience
 end
