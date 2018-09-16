77b87700cf4d675fa471b0e373baf1b718b930ee
dongmy54 <615760433@qq.com>
2018-09-16 16:37:16 +0800

    add account_sessions controller

---
+++ app/controllers/enterprise/account_sessions_controller.rb
@@ -0,0 +1,12 @@
+class Enterprise::AccountSessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
---
+++ app/helpers/enterprise/account_sessions_helper.rb
@@ -0,0 +1,2 @@
+module Enterprise::AccountSessionsHelper
+end
